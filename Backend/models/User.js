e55f318f177e9598a0ccc5ec44b2337c223230e8
nsikakudoh <nsikak@sparknerds.io>
2022-10-12 09:00:19 -0600

    create user schema

---
+++ Backend/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+constuserSchema = new Schema({
+    
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+});
+
+// export to MongoDB stored as users
+
+export default mongoose.model('User', Schema);
