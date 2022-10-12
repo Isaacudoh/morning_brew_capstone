faaf036b277d2614a01678bf5b8adab64e1f96be
nsikakudoh <nsikak@sparknerds.io>
2022-10-12 08:41:06 -0600

    Database Connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello, Nsikak!");
 });
 
-app.listen(8080);
+mongoose.connect("mongodb+srv://admin:Caseis$25@cluster0.3f7a7pr.mongodb.net/morning_brew?retryWrites=true&w=majority").then(()=>app.listen(8080)).then(()=>console.log ("Connected to Database and listenting to local host on Port 8080")).catch((err) =>console.log(err));
+
+
+
+// Caseis$25
