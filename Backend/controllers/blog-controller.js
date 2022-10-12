41fa3ed18dbc5bb894ffed817bfc48a2f99d4088
nsikakudoh <nsikak@sparknerds.io>
2022-10-12 13:36:23 -0600

    blog

---
+++ Backend/controllers/blog-controller.js
@@ -0,0 +1,14 @@
+import Blog from "../models/Blog";
+
+const getAllBlogs = async (req, res) => {
+  let blogs;
+  try {
+    blogs = await Blog.find();
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!blogs) {
+    return res.status(404).json({ Message: "No Blog found" });
+  }
+  return res.status(200).json({ blogs });
+};
