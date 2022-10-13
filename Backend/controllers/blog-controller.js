1047ea423469f627e9037be5dacdbd727a58c628
nsikakudoh <nsikak@sparknerds.io>
2022-10-13 10:07:38 -0600

    delete blog from user profile

--- Backend/controllers/blog-controller.js
+++ Backend/controllers/blog-controller.js
@@ -82,7 +82,9 @@ export const deleteBlog = async (req, res) => {
   const id = req.params.id;
   let blog;
   try {
-    blog = await Blog.findByIdAndRemove(id);
+    blog = await Blog.findByIdAndRemove(id).populate("user");
+    await blog.user.blogs.pull(blog);
+    await blog.user.save();
   } catch (err) {
     console.log(err);
   }
