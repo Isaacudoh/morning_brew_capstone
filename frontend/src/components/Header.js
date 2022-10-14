36d19ecb9ddbccc6f92750350597f3c0c07c2a12
nsikakudoh <nsikak@sparknerds.io>
2022-10-14 09:50:35 -0600

    links usestate

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,27 @@
-import React from "react";
-import { Toolbar, AppBar, Typography, Box } from "@mui/material";
+import React, { useState } from "react";
+import { Tab, Tabs, Toolbar, AppBar, Typography, Box } from "@mui/material";
 
 const Header = () => {
+  const [value, setValue] = useState();
   return (
-    <AppBar sx={{background: "Black"}}>
+    <AppBar position="sticky" sx={{ background: "Black" }}>
       <Toolbar>
         <Typography variant="h4">Morning Brew</Typography>
+        <Box display="flex">
+          <Tabs
+            value={value}
+            onChange={(e,val) => setValue(val)}
+            textColor="white"
+          >
+            <Tab label="Articles" />
+            <Tab label="My Articles" />
+            <Tab label="Add Article" />
+          </Tabs>
+        </Box>
         <Box display="flex" marginLeft="auto">
           <button>Login</button>
-          <button>Start</button>
+          <button>Signup</button>
+          <button>Logout</button>
         </Box>
       </Toolbar>
     </AppBar>
