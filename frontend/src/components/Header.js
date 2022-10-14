d757348273cbf0fec901feee4016491b8382aa5a
nsikakudoh <nsikak@sparknerds.io>
2022-10-14 10:43:57 -0600

    link routes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
-import { Tab, Tabs, Toolbar, AppBar, Typography, Box } from "@mui/material";
+import { Link, Tab, Tabs, Toolbar, AppBar, Typography, Box } from "@mui/material";
 
 const Header = () => {
   const [value, setValue] = useState();
   return (
     <AppBar position="sticky" sx={{ background: "Black" }}>
       <Toolbar>
-        <Typography variant="h4">Morning Brew</Typography>
-        <Box display="flex">
+        <Typography variant="h4">Morning Br☕w </Typography>
+        <Box display="flex" marginLeft={"auto"} marginRight="auto">
           <Tabs
             value={value}
-            onChange={(e,val) => setValue(val)}
+            onChange={(e, val) => setValue(val)}
             textColor="white"
           >
-            <Tab label="Articles" />
-            <Tab label="My Articles" />
-            <Tab label="Add Article" />
+            <Tab label="Articles" href="/blogs"/>
+            <Tab label="My Articles" href="/myBlogs"/>
+          
           </Tabs>
         </Box>
         <Box display="flex" marginLeft="auto">
-          <button>Login</button>
+          <button LinkComponent={Link} to="/auth"> Login</button>
           <button>Signup</button>
           <button>Logout</button>
         </Box>
