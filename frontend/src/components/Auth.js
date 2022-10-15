583071cdba176d8159ca28145244408c1ae8d00d
nsikakudoh <nsikak@sparknerds.io>
2022-10-15 08:19:59 -0600

    complete email & password

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, inputClasses, TextField, Typography } from "@mui/material";
 
 const Auth = () => {
+  const [inputs, setInputs] = useState({
+    name: "", email: "", password: "",
+  });
   const [isSignup, setIsSignup] = useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState)=> ({
+      prevState,
+      [e.target.name] : e.target.value
+    }));
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Box
           maxWidth={400}
           display="flex"
@@ -22,11 +35,11 @@ const Auth = () => {
             {isSignup ? <h2>Signup</h2> : <h2>Login</h2>}
           </Typography>
           {isSignup && (
-            <TextField placeholder="Enter your name" margin="normal" />
+            <TextField name="name" onChange={handleChange} value={inputs.name}placeholder="Enter your name" margin="normal" />
           )}
-          <TextField placeholder="Enter your email" margin="normal" />
-          <TextField placeholder="Enter your password" margin="normal" />
-          <Button>Submit 🚀</Button>
+          <TextField name="email" onChange={handleChange} value={inputs.email}placeholder="Enter your email" margin="normal" />
+          <TextField name="password" onChange={handleChange} value={inputs.password}placeholder="Enter your password" margin="normal" />
+          <Button type="submit"> Submit 🚀</Button>
           <Button onClick={() => setIsSignup(!isSignup)}>
             New here? {isSignup ? "Login" : "Signup"}
           </Button>
