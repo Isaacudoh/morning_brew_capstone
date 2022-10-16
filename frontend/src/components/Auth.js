40fe9f2c329fadabba86c75c29240d3a126ce093
nsikakudoh <nsikak@sparknerds.io>
2022-10-16 15:52:58 -0600

    Auth complete

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,21 +1,48 @@
 import React, { useState } from "react";
-import { Box, Button, inputClasses, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  inputClasses,
+  TextField,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 
 const Auth = () => {
   const [inputs, setInputs] = useState({
-    name: "", email: "", password: "",
+    name: "",
+    email: "",
+    password: "",
   });
   const [isSignup, setIsSignup] = useState(false);
   const handleChange = (e) => {
-    setInputs((prevState)=> ({
-      prevState,
-      [e.target.name] : e.target.value
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
     }));
-  }
+  };
+
+  const sendRequest = async (type = "login") => {
+    const res = await axios
+      .post(`http://localhost:8080/users/${type}`, {
+        name: inputs.name,
+        email: inputs.email,
+        password: inputs.password,
+      })
+      .catch((err) => console.log(err));
+
+    const data = await res.data;
+    return data;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputs);
+    if (isSignup) {
+      sendRequest("signup").then((data) => console.log(data));
+    } else {
+      sendRequest().then((data) => console.log(data));
+    }
   };
   return (
     <div>
@@ -36,10 +63,28 @@ const Auth = () => {
             {isSignup ? <h2>Signup</h2> : <h2>Login</h2>}
           </Typography>
           {isSignup && (
-            <TextField name="name" onChange={handleChange} value={inputs.name}placeholder="Enter your name" margin="normal" />
+            <TextField
+              name="name"
+              onChange={handleChange}
+              value={inputs.name}
+              placeholder="Enter your name"
+              margin="normal"
+            />
           )}
-          <TextField name="email" onChange={handleChange} value={inputs.email}placeholder="Enter your email" margin="normal" />
-          <TextField name="password" onChange={handleChange} value={inputs.password}placeholder="Enter your password" margin="normal" />
+          <TextField
+            name="email"
+            onChange={handleChange}
+            value={inputs.email}
+            placeholder="Enter your email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            value={inputs.password}
+            placeholder="Enter your password"
+            margin="normal"
+          />
           <Button type="submit"> Submit 🚀</Button>
           <Button onClick={() => setIsSignup(!isSignup)}>
             New here? {isSignup ? "Login" : "Signup"}
